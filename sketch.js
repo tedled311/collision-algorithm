9991cccc2e275d4b5fe4cb8b061d2170a73bb90f
tedled311 <78272891+tedled311@users.noreply.github.com>
2021-02-13 10:18:49 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var fixedrect, movingrect
+
+function setup() {
+  createCanvas(1200,800);
+  fixedrect=createSprite(600, 400, 50, 80);
+  fixedrect.shapeColor="black";
+  fixedrect.debug=true
+  movingrect=createSprite(400,200,80,30)
+  movingrect.shapeColor="blue"
+  movingrect.debug=true
+
+}
+
+function draw() {
+  background("white");
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+  if(movingrect.x-fixedrect.x < fixedrect.width/2+movingrect.width/2
+    && fixedrect.x-movingrect.x < fixedrect.width/2+movingrect.width/2
+   && movingrect.y-fixedrect.y < fixedrect.height/2+movingrect.height/2
+   && fixedrect.y-movingrect.y < fixedrect.height/2+movingrect.height/2 )  {
+     movingrect.shapeColor="red"
+     fixedrect.shapeColor= "red"
+   }
+   else{
+    movingrect.shapeColor="orange"
+    fixedrect.shapeColor= "orange"
+   }
+  drawSprites();
+}
